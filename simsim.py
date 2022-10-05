756f90087c0b85ab1599c581ea0a69bcf8bed539
lee-sihun <lee.sihun040223@gmail.com>
2022-10-06 07:24:41 +0900

    유사도 비교 추가

--- simsim.py
+++ simsim.py
@@ -1,11 +1,10 @@
-from tracemalloc import start
 import check as ch
 import server
 
 conn = server.db_connect()
 
 check_text = '!가르치기 '
-similar_value = 0.7
+similar_value = 0.6
 
 start_list = []
 end_list = []
@@ -38,19 +37,22 @@ while True:
         print('가르칠 대답을 입력해주세요.')
 
         teach_text_end = input()
-        # start_list.append(teach_text_start)
-        # end_list.append(teach_text_end)
         server.insert_data(conn, teach_text_start, teach_text_end)
         print('대화를 가르쳤습니다.')
         continue
 
     check = False
+    word_list = []
+    word_sim = []
     for i in range(len(start_list)-1, -1, -1):
         if ch.Similarity_Check(start_list[i], message) > similar_value:
-            result = end_list[i]
-            print(result)
-            check = True
-            break
+            word_list.append(end_list[i])
+            word_sim.append(ch.Similarity_Check(start_list[i], message))
+
+    if len(word_list) != 0:
+        result = word_list[word_sim.index(max(word_sim))]
+        check = True
+        print(result)
 
     if check == False:
-        print('잘 모르는 대화입니다.')
+        print('잘 모르는 대화입니다. 가르쳐주세요.')
