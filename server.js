0b0bafe3570eaee8da173eea17be6c18b69ccef2
lee-sihun <lee.sihun040223@gmail.com>
2022-10-06 11:07:26 +0900

    server.js finish

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ const pool = mariadb.createPool({
   password: "0000",
 });
 
-async function search_data(pool) {
+const insert_data = async (pool, qs, ans) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+
+    const result = await conn.query("INSERT INTO Text VALUES(?, ?)", [qs, ans]);
+    console.log(result);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release(); // release pool
+  }
+};
+
+const search_data = async (pool) => {
   let conn;
   try {
     conn = await pool.getConnection();
@@ -19,9 +33,9 @@ async function search_data(pool) {
   } finally {
     if (conn) conn.release(); // release pool
   }
-}
+};
 
-function ext_data(data) {
+const ext_data = (data) => {
   var qs = [];
   var ans = [];
   for (i = 0; i <= data.length; i++) {
@@ -47,6 +61,7 @@ function ext_data(data) {
       console.log(err);
     }
   });
-}
+};
 
-search_data(pool).then((response) => ext_data(response)); // 원하는 값인 data를 출력할 수 있습니다
+// search_data(pool).then((response) => ext_data(response)); // 원하는 값인 data를 출력할 수 있습니다
+// insert_data(pool, "js test", "good!");
